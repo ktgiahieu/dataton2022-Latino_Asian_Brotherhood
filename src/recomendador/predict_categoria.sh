21ef7be2f574f6e3ac31b290f055288317d390c1
ktgiahieu <ktgiahieu@gmail.com>
2022-11-06 04:50:37 +0100

    fix code bug

--- src/recomendador/predict_categoria.sh
+++ src/recomendador/predict_categoria.sh
@@ -2,5 +2,5 @@
 
 cd src/deliverable_categorizacion
 python3 3-Infer_participacion_classifier.py
-python3 5-Infer_categoria_classifier copy.py
+python3 5-Infer_categoria_classifier.py
 python3 6-Combine_participacion_categoria.py
