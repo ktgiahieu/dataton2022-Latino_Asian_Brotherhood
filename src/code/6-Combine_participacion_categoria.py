4ee2f25e5e9fd69f1901d905f7ede53216d5f633
ktgiahieu <ktgiahieu@gmail.com>
2022-11-14 22:29:44 +0100

    a

--- src/code/6-Combine_participacion_categoria.py
+++ src/code/6-Combine_participacion_categoria.py
@@ -4,16 +4,12 @@ import numpy as np
 import pandas as pd
 import os
 import regex
-import pickle
 
-from sentence_transformers import SentenceTransformer
-from scipy import spatial
+from datasets import load_dataset
+from sentence_transformers.losses import CosineSimilarityLoss
+from setfit import SetFitModel, SetFitTrainer
 
-CATEGORIA_CLASSIFICATION_THRESHOLD = 0.7
-
-def get_cosine_similarity(vector1, vector2):
-    sim = 1 - spatial.distance.cosine(vector1, vector2)
-    return sim
+CLASSIFICATION_THRESHOLD = 0.7
 
 def match_and_extract(row, *, text_col):
     pattern = r'\b(?<=(.{0,100}))('+'(.{0,20})'.join(row['trimmed_name'].split())+r')\b(?=(.{0,100}))'
@@ -31,7 +27,7 @@ def get_paricipacion(row):
             return 'No aplica'
 
 def get_categoria(row):
-    if np.max(row[category_columns].values)>CATEGORIA_CLASSIFICATION_THRESHOLD and row.pred_categoria != 'Descartable':
+    if np.max(row[category_columns].values)>CLASSIFICATION_THRESHOLD and row.pred_categoria != 'Descartable':
         return row.pred_categoria
     else:
         if row.participacion != 'No aplica':
@@ -50,39 +46,39 @@ def set_seed(seed=1234):
 set_seed(2022)
 
 if __name__ == "__main__":
+    # IMPORTANT: Fix missing values in clientes.csv
+    client_df = pd.read_csv('../data/archivos_auxiliares/clientes.csv')
+    new_row = {'nit':860516431, 
+               'nombre':'GRAN TIERRA ENERGY COLOMBIA', 
+               'desc_ciiu_division':'EXTRACCION DE PETROLEO CRUDO Y GAS NATURAL', 
+               'desc_ciuu_grupo':'EXTRACCION DE PETROLEO CRUDO',
+               'desc_ciiuu_clase': 'EXTRACCION DE PETROLEO CRUDO',
+               'subsec': 'EXTRACCION DE PETROLEO'
+              }
+    client_df = client_df.append(new_row, ignore_index=True)
+
     # ------ 1. TRIM CLIENTS' NAME ---------
     stopwords = pd.read_csv('../data/archivos_auxiliares/stopwords.txt', names=['stopword'], header=None)
     stopwords = stopwords['stopword']
     stopwords = stopwords.str.replace('.', r'\.', regex=False)
 
-    clients = pd.read_csv('../data/archivos_auxiliares/clientes.csv', usecols=['nombre'])
-    clients['trimmed_name'] = clients['nombre']
+    client_df['trimmed_name'] = client_df['nombre']
     for word in stopwords:
-        clients['trimmed_name'] = clients['trimmed_name'].str.replace(r'(?:\s|^){}(?:\s|$)'.format(word), ' ', regex=True).str.strip()
-
-    clients.to_csv('../data/archivos_auxiliares/clean_names.csv', index=False)
-
-
+        client_df['trimmed_name'] = client_df['trimmed_name'].str.replace(r'(?:\s|^){}(?:\s|$)'.format(word), ' ', regex=True).str.strip()
 
     # ------ 2. FIND ALL MATCHES OF CLIENTS IN THE NEWS ---------
     regex_chars = ['.','(', ')']
-    client_df = pd.read_csv('../data/archivos_auxiliares/clientes.csv')
-
+    for char in regex_chars:
+        client_df['trimmed_name'] = client_df['trimmed_name'].str.replace(char, f'\{char}', regex=False)
+        
     # regroup_desc_ciiu_division.csv is a manually created CSV file, 
     # which have bigger groups than desc_ciiu_division
     regroup_desc_ciiu_division = pd.read_csv('../data/archivos_auxiliares/regroup_desc_ciiu_division.csv')
     client_df = pd.merge(client_df, regroup_desc_ciiu_division, how='left', on='desc_ciiu_division')
     client_df['group'] = 'group_' + client_df['group'].astype(str)
-    clean_names = pd.read_csv('../data/archivos_auxiliares/clean_names.csv')
-    client_df = client_df.merge(clean_names, on='nombre')
-    for char in regex_chars:
-        client_df['trimmed_name'] = client_df['trimmed_name'].str.replace(char, f'\{char}', regex=False)
-
-
-    client_news_df = pd.read_csv('../data/archivos_auxiliares/clientes_noticias.csv')[['nit', 'news_url_absolute','news_id']]
+        
+    client_news_df = pd.read_csv('../data/archivos_auxiliares/clientes_noticias.csv')[['nit','news_id']]
     news_df = pd.read_csv('../data/archivos_auxiliares/noticias.csv')
-
-
     client_news_df = client_news_df.merge(news_df, on='news_id')
     client_news_df = client_news_df.merge(client_df, on='nit')
 
@@ -96,22 +92,15 @@ if __name__ == "__main__":
     client_news_df['appearance_in_title'] = client_news_df['appearance_in_title'].map(str)
     client_news_df['appearance_in_body'] = client_news_df['appearance_in_body'].map(str)
 
-    client_news_df = client_news_df[['nit', 'news_id', 'nombre', 'desc_ciiu_division', 'trimmed_name', 'name_in_news']]
+    client_news_df = client_news_df[['nit', 'news_id', 'nombre', 'desc_ciiu_division', 'group', 'trimmed_name', 'name_in_news']]
 
     # ------ 3. LOAD PREDICTIONS OF "paricipacion" AND "categoria" ---------
+
+    pred_group = pd.read_csv('../data/intermediate_output/pred_news_group.csv')
     pred_category = pd.read_csv('../data/intermediate_output/pred_news_categoria.csv')
-    with open('../data/intermediate_output/news_embeddings.pkl', 'rb') as handle:
-        news_embeddings = pickle.load(handle)
 
-
-
-
-
-
-
-    division_similarity = [get_cosine_similarity(x) for x in news_embeddings]
-
-    client_news_df['division_similarity'] = client_news_df
+    group_columns = ['group_30', 'group_40', 'group_28', 'group_29', 'group_20', 'group_25', 'group_1', 'group_32', 'group_16', 'group_23', 'group_11', 'group_22', 'group_19', 'group_39', 'group_49', 'group_2', 'group_44', 'group_18', 'group_13', 'group_26', 'group_41', 'group_36', 'group_31', 'group_14', 'group_12', 'group_27', 'group_43', 'group_21', 'group_7', 'group_46', 'group_35', 'group_48', 'group_33', 'group_38', 'group_5', 'group_4', 'group_47', 'group_6', 'group_42']
+    pred_group = pred_group[['nit', 'news_id', 'pred_group'] + group_columns]
 
     category_columns = ['Macroeconomía', 'Sostenibilidad', 'Innovación', 'Regulaciones', 'Alianza', 'Reputación', 'Descartable']
     pred_category = pred_category[['nit', 'news_id', 'preds', 'pred_categoria'] + category_columns]
@@ -124,3 +113,22 @@ if __name__ == "__main__":
 
     pred_df['nombre_equipo'] = 'Latino-Asian Brotherhood'
     pred_df[['nombre_equipo', 'nit', 'news_id', 'participacion', 'categoria']].to_csv('../data/output/categorizacion.csv', index=False)
+
+    # ------ 5. CREATE recomendacion.csv ---------   
+    # Load the source ranking file
+    sources_ranking = pd.read_csv('dataton2022-Latino_Asian_Brotherhood/src/data/archivos_auxiliares/sources_ranking.csv')
+    pred_df['news_url_header'] = pred_df.news_url_absolute.apply(lambda x:re.findall('(https?://.+?)/', x)[0])
+    pred_df = pred_df.merge(sources_ranking, how='left', on='news_url_header')
+    pred_df.score = pred_df.score.fillna(0.1)
+
+    # Get the score of the model as group_proba
+    pred_df['group_proba'] = pred_df.apply(lambda row: row[row['group']], 1)
+    
+    # The recomendacion_score is that confidence of the model above * source score
+    pred_df['recomendacion_score'] = pred_df['group_proba'] * pred_df['score']
+    pred_df['recomendacion'] = pred_df.groupby('nit').recommender_score.rank(ascending=False)
+
+    pred_df = pred_df.sort_values(['nit','recomendacion'],ascending=True).group('nit').head(5)
+
+    pred_df[['nombre_equipo', 'nit', 'news_id', 'participacion', 'categoria', 'recomendacion']].to_csv('recomendacion.csv')
+
