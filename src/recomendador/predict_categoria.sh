21d53594fc74bb9b4352dc7d746ccca8836290eb
ktgiahieu <ktgiahieu@gmail.com>
2022-11-14 22:01:49 +0100

    a

--- src/recomendador/predict_categoria.sh
+++ src/recomendador/predict_categoria.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd src/deliverable_categorizacion
+cd src/code
 python3 3-Infer_participacion_classifier.py
 python3 5-Infer_categoria_classifier.py
 python3 6-Combine_participacion_categoria.py
--- src/recomendador/train_categoria.sh
+++ src/recomendador/train_categoria.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd src/deliverable_categorizacion
+cd src/code
 python3 0-Get_news_embeddings.py
 python3 1-Label_participacion.py
 python3 2-Train_participacion_classifier.py
